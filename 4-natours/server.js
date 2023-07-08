64d7a649c7f449555a5e254e52b979d2339bfec8
Renven <reven666@gmail.com>
2023-07-08 15:09:38 +0800

    feat: finished simply api

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,7 +1,25 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+// const DB = process.env.DATABASE;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) => console.log(err));
 
 const port = 3000 || process.env.PORT;
 app.listen(port, () => {
